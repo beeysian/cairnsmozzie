7aa27dc79c653af6d7f3288da069eb04675f1c4a
Sarah Belet <sarah@ua-eduroam-ten-25-136-50.uniaccess.unimelb.edu.au>
2019-06-17 19:54:28 +1000

    beginning of grid/microclimate stuff

---
+++ R/tempdev_functions.R
@@ -0,0 +1,15 @@
+#' Given a latitude and longitude, determines the corresponding grid ID.
+#' Assumes grid.df is a global variable.
+#' @param testlat Latitude being considered.
+#' @param testlong Longitude being considered.
+#' @return Corresponding grid ID number.
+get_gridID <- function(testlat, testlong){
+  closelong <- which(abs(grid.df$long - testlong) == min(abs(grid.df$long - testlonf)))
+  if(length(closelong) > 1){
+    closelat <- closelong[which(abs(grid.df$lat[closelong] - testlat) == min(abs(grid.df$lat[closelong] - testlat)))]
+  }
+  else if(length(closelong) == 0){
+    closelat <- closelong
+  }
+return(closelat)
+}
